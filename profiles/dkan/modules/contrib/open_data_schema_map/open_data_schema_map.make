8ad509f8fc69793c3e144a11f3df50e21e0cceb1
Mark Vaughn <mvaughn@slginnovation.com>
2017-01-11 19:43:38 -0800

    DKAN Prod Sandbox Commit

---
+++ profiles/dkan/modules/contrib/open_data_schema_map/open_data_schema_map.make
@@ -0,0 +1,12 @@
+core = 7.x
+api = 2
+
+libraries[symfonyserializer][type] = libraries
+libraries[symfonyserializer][download][type] = "git"
+libraries[symfonyserializer][download][url] = "https://github.com/symfony/serializer.git"
+libraries[symfonyserializer][download][revision] = a6e5282edbfacc113653ed0224c2a92204ebc5ba
+
+libraries[json-schema][type] = libraries
+libraries[json-schema][download][type] = "git"
+libraries[json-schema][download][url] = "https://github.com/justinrainbow/json-schema.git"
+libraries[json-schema][download][tag] = 1.3.7
---
+++ sites/all/themes/corp/templates/block.tpl.php
@@ -0,0 +1,17 @@
+<div id="block-<?php print $block->module .'-'. $block->delta ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <div class="block-inner">
+<?php global $base_url; ?>
+    <?php print render($title_prefix); ?>
+    <?php if ($block->subject): ?>
+      <h2 class="block-title"<?php print $title_attributes; ?>><?php print $block->subject ?><img src="<?php print $base_url; ?>/sites/all/themes/corporate/images/bullet.png"></h2>
+    <?php endif;?>
+    <?php print render($title_suffix); ?>
+ <div style="clear:both;"></div>
+  </div>
+
+    <div class="content" <?php print $content_attributes; ?>>
+      <?php print $content; ?>
+      
+    </div>
+ 
+</div> <!-- /block-inner /block -->
